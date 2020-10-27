fe58128c3929b8c481c9d551f9a7cc2844745c2b
julien_calvignac <j.calvignac@laposte.net>
2020-10-27 16:16:22 +0100

    fin exo boucles

---
+++ BoucleApp/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoucleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Exo 1 :
+            Console.WriteLine("Exo 1 :");
+            Console.ReadLine();
+
+            Console.WriteLine("Boucle for :");
+
+            int i = 0;
+            for (i = 0; i < 21; i++)
+            {
+                if(i%2 == 0) { Console.WriteLine($"{i}"); };
+
+            }
+
+            Console.ReadLine();
+
+            Console.WriteLine("Boucle while :");
+
+            i = 0;
+
+            while (i < 21)
+            {
+                if (i % 2 == 0) { Console.WriteLine($"{i}"); };
+                i++;
+            }
+
+            Console.ReadLine();
+            
+            //Exo 2 :
+            Console.WriteLine("Exo 2 :");
+            Console.ReadLine();
+
+            Console.WriteLine("Ecrivez un nombre :");
+            int k = int.Parse(Console.ReadLine());
+
+            int compte = 0;
+
+            for(int j = 1; j < k + 1; j++)
+            {
+                compte += j;
+            }
+
+            Console.WriteLine($"Compte = {compte}");
+            Console.ReadLine();
+            
+            //Exo 3 :
+            Console.WriteLine("Exo 3 :");
+            Console.ReadLine();
+
+            Console.WriteLine("Veuillez entrer un premier nombre :");
+            int nombre1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez entrer un deuxième nombre :");
+            int nombre2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez entrer un troisième nombre :");
+            int nombre3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez entrer un quatrième nombre :");
+            int nombre4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez entrer un cinquième nombre :");
+            int nombre5 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"La somme des nombres est {nombre1 + nombre2 + nombre3 + nombre4 + nombre5}");
+            Console.WriteLine($"La moyenne des nombres est {(nombre1 + nombre2 + nombre3 + nombre4 + nombre5)/5}");
+
+            Console.ReadLine();
+            
+            //Exo 4 :
+            Console.WriteLine("Exo 4 :");
+            Console.ReadLine();
+
+            Console.WriteLine("Veuillez entrer un premier nombre :");
+            int val1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez entrer un deuxième nombre :");
+            int val2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"La somme des nombres est : {StaticClass.CalculSommeEntiers(val1, val2)}");
+
+            Console.ReadLine();
+            
+            //Exo 5 :
+            Console.WriteLine("Exo 5 :");
+            Console.ReadLine();
+
+            List<decimal> listeNum = new List<decimal>();
+
+            Console.WriteLine("Veuillez entrer un premier nombre :");
+            listeNum.Add(decimal.Parse(Console.ReadLine()));
+            Console.WriteLine("Veuillez entrer un deuxième nombre :");
+            listeNum.Add(decimal.Parse(Console.ReadLine()));
+            Console.WriteLine("Veuillez entrer un troisième nombre :");
+            listeNum.Add(decimal.Parse(Console.ReadLine()));
+            Console.WriteLine("Veuillez entrer un quatrième nombre :");
+            listeNum.Add(decimal.Parse(Console.ReadLine()));
+            Console.WriteLine("Veuillez entrer un cinquième nombre :");
+            listeNum.Add(decimal.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"La moyenne des nombres est : {StaticClass.CalculMoyenne(listeNum)}");
+
+            Console.ReadLine();
+            */
+            //Exo 6 :
+            Console.WriteLine("Exo 6 :");
+            Console.ReadLine();
+
+            Console.WriteLine($"La somme des nombres de la liste commune est : {StaticClass.LastMethode()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BoucleApp/StaticClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoucleApp
+{
+    class StaticClass
+    {
+        public static int Addition(params int[] list)
+        {
+            int somme = 0;
+            foreach(int val in list)
+            {
+                somme += val;
+            }
+            return somme;
+        }
+
+        public static int CalculSommeEntiers(int x, int y)
+        {
+            int somme = 0;
+            for (int i = x; i < y + 1; i++)
+            {
+                somme += i;
+            }
+            return somme;
+        }
+
+        public static decimal CalculMoyenne(List<decimal> list)
+        {
+            decimal moyenne = 0;
+
+            foreach(decimal num in list)
+            {
+                moyenne += num;
+            }
+
+            moyenne = moyenne / list.Count;
+
+            return moyenne;
+        }
+
+        public static int LastMethode()
+        {
+            int somme = 0;
+
+            List<int> multiples3 = new List<int>();
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0) { multiples3.Add(i); }
+            }
+
+            List<int> multiples5 = new List<int>();
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 5 == 0) { multiples5.Add(i); }
+            }
+
+            foreach (int number3 in multiples3)
+            {
+                foreach (int number5 in multiples5)
+                {
+                    if (number3 == number5 )
+                    {
+                        Console.WriteLine($"liste 3 = {number3}, liste 5 = {number5}");
+                        somme += number3 + number5;
+                    }
+                }
+            }
+
+            return somme;
+        }
+    }
+}
